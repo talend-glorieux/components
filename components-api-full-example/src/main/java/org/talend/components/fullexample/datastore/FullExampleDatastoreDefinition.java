115f7d16ba2c0fef942a1750404cde3558bd6198
Sebastien Gandon <sgandon@talend.com>
2016-10-21 16:03:38 +0200

    feat (TCOMP-256):made RuntimeableRuntime generic and removed DatasetRuntime

--- components-api-full-example/src/main/java/org/talend/components/fullexample/datastore/FullExampleDatastoreDefinition.java
+++ components-api-full-example/src/main/java/org/talend/components/fullexample/datastore/FullExampleDatastoreDefinition.java
@@ -2,13 +2,16 @@
 package org.talend.components.fullexample.datastore;
 
 import org.talend.components.api.component.runtime.RuntimeInfo;
+import org.talend.components.common.dataset.DatasetProperties;
 import org.talend.components.common.datastore.DatastoreDefinition;
+import org.talend.components.common.datastore.DatastoreProperties;
 import org.talend.daikon.SimpleNamedThing;
 
 /**
  * An example of a DatastoreDefinition.
  */
-public class FullExampleDatastoreDefinition extends SimpleNamedThing implements DatastoreDefinition<FullExampleDatastoreProperties> {
+public class FullExampleDatastoreDefinition extends SimpleNamedThing
+        implements DatastoreDefinition<FullExampleDatastoreProperties> {
 
     private static final String NAME = "FullExampleDatastore";
 
@@ -18,11 +21,16 @@ public class FullExampleDatastoreDefinition extends SimpleNamedThing implements 
 
     @Override
     public FullExampleDatastoreProperties createProperties() {
-        return null;
+        return new FullExampleDatastoreProperties(null);
     }
 
     @Override
     public RuntimeInfo getRuntimeInfo(FullExampleDatastoreProperties properties, Object ctx) {
+        return null;
+    }
+
+    @Override
+    public DatasetProperties createDatasetProperties(DatastoreProperties storeProp) {
         return null;
     }
 }
--- components-common/src/main/java/org/talend/components/common/datastore/runtime/DatastoreRuntime.java
+++ components-common/src/main/java/org/talend/components/common/datastore/runtime/DatastoreRuntime.java
@@ -12,36 +12,12 @@
 // ============================================================================
 package org.talend.components.common.datastore.runtime;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.talend.components.api.component.runtime.RuntimableRuntime;
 import org.talend.components.api.container.RuntimeContainer;
-import org.talend.components.api.properties.ComponentProperties;
-import org.talend.daikon.NamedThing;
+import org.talend.components.common.datastore.DatastoreProperties;
 import org.talend.daikon.properties.ValidationResult;
 
-public interface DatastoreRuntime extends RuntimableRuntime {
-
-    /**
-     * Get the list of possible dataset names available for this {@code Datastore} or an empty List if none.
-     * This method can cope with a tree of choices to access to the datasets as long as they can be
-     * represented with a string.
-     * for example in Cassadra a call to
-     * <code>getPossibleDatasetNames(container, null)</code>
-     * will return the list of all keyspaces, then a call to
-     * <code>getPossibleDatasetNames(container, "keyspaceOne")</code>
-     * will return all the table associated tables related to *keyspaceOne*.
-     * <p>
-     * This uses the {@link ComponentProperties} previously specified to make any necessary connection.
-     * It is not intended to update any associated
-     * {@code ComponentProperties} object.
-     * 
-     * @param container give information of the container running this class.
-     * @param datasetPath, helps locate the dataset in a tree architecture, a "/" will be use to separate each
-     *            path. May be null if you want to retrieve root dataset path.
-     */
-    List<NamedThing> getPossibleDatasetNames(RuntimeContainer container, String datasetPath) throws IOException;
+public interface DatastoreRuntime extends RuntimableRuntime<DatastoreProperties> {
 
     /**
      * perform a series of health checks like cheking the connection is possible or the status of each clusters.
